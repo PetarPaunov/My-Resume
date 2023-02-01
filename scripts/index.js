df18594118a2f55a64ad6bd3059f1998c1f320df
PetarPaunov <petarp.2000@abv.bg>
2023-02-01 22:28:02 +0200

    added article for certificate

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,14 @@ window.onscroll = function() {
     navbar.classList.remove('nav-active')
   }
 }
+
+document.querySelectorAll('.certificates .certificate .image .img').forEach(image => {
+  image.onclick = () => {
+    document.querySelector('.main .certificates .certificate .popup-image').style.display = 'block';
+    document.querySelector('.popup-image img').src = image.getAttribute('src');
+  }
+});
+
+document.querySelector('.popup-image span').onclick = () => {
+  document.querySelector('.main .certificates .certificate .popup-image').style.display = 'none';
+}
